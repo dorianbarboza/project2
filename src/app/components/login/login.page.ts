b344a89279fe2fde54522abef1fb8f4bf5bd1cd4
dorianbarboza <dorianbarboza@gmail.com>
2019-05-24 03:26:44 -0500

    2. vie 24.mayo.2019

--- src/app/components/login/login.page.ts
+++ src/app/components/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service"
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private authService:AuthService, public router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin()
+  {
+   console.log("Precionado");
+   this.authService.login(this.email, this.password).then(res => {
+     this.router.navigate(['/home']);
+   }).catch(err => alert('Error'))
   }
 
 }
